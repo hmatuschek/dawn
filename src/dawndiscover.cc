0562afe44cbf3285c7f6f2b7e70478449a5b70e3
Hannes Matuschek <hmatuschek@gmail.com>
2017-01-15 12:57:39 +0100

    Fixed debug.

--- src/dawndiscover.cc
+++ src/dawndiscover.cc
@@ -54,7 +54,7 @@ DawnDiscover::start() {
   }
   std::cerr << "Scan for devices..." << std::endl;
   foreach (QBluetoothDeviceInfo info, _discovery.discoveredDevices()) {
-    qDebug() << info.address() << ": " << info.name();
+    std::cerr << info.address().toString().toStdString() << ": " << info.name().toStdString();
   }
   _discovery.start();
   return true;
